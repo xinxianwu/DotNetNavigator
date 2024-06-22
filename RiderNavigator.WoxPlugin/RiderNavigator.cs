79c85c7057ec2d069373be2e7f7cd0a6e3282e74
Xinxian <xinxian.wu@onelab.tw>
2024-06-22 15:36:46 +0800

    init proejct

---
+++ RiderNavigator.WoxPlugin/RiderNavigator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Wox.Plugin;
+
+namespace RiderNavigator.WoxPlugin
+{
+    public class RiderNavigator : IPlugin
+    {
+        public List<Result> Query(Query query)
+        {
+            return new List<Result>()
+            {
+                new Result
+                {
+                    Title = "Project1",
+                    SubTitle = "Path1",
+                    IcoPath = "rider.ico",
+                    Action = context => true
+                },
+                new Result
+                {
+                    Title = "Project1",
+                    SubTitle = "Path1",
+                    IcoPath = "rider.ico",
+                    Action = context => true
+                },
+            };
+        }
+
+        public void Init(PluginInitContext context)
+        {
+            // throw new System.NotImplementedException();
+        }
+    }
+}
